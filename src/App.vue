8e511d649a60fd865c3050bc04314864e9cc026b
abdubosit-js <abdubositjs@gmail.com>
2022-10-28 23:51:50 +0500

    brinchi commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="container">
+
+		<div class="person_wrapper" v-for="person in persons" v-bind:key="person.id">
+			<div><b>name: </b>{{person.name}}</div>
+			<div><b>email: </b> {{person.email}} </div>
+			<div><b>description: </b>{{person.desc}}</div>
+		</div>
+
+		<!-- counter -->
+		
+		<!-- <button @click="decrement">-</button>
+		<h1>{{count}}</h1>
+		<button @click="increment" >+</button> -->
+	</div>
 </template>
 
+<script>
+	export default {
+		data() {
+			return {
+				persons: [
+					{
+						id: 1,
+						name: "abdubosit",
+						email: "abdubositjs@gmail.com",
+						desc: "lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor"
+					},
+					{
+						id: 2,
+						name: "nosir",
+						email: "nosir5001@gmail.com",
+						desc: "lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor"
+					},
+					{
+						id: 3,
+						name: "abduvali",
+						email: "abduvali@gmail.com",
+						desc: "lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor"
+					}
+				]
+				// count: 0,
+			}
+		},
+		methods: {
+		
+		
+		
+		/*	increment() {
+				this.count += 1
+			},
+			decrement() {
+				this.count -= 1
+			}
+		*/
+		}
+	}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: sans-serif;
+	}
+	.container {
+		padding: 50px;
+		width: 100%;
+		height: 100vh;
+		background: rgb(180, 180, 255);
+	}
 
-nav {
-  padding: 30px;
-}
+	.person_wrapper {
+		border: 1px solid gray;
+		padding: 5px 10px;
+		margin-bottom: 10px;
+	}
+	/* button {
+		padding: 8px 24px;
+		cursor: pointer;
+		font-size: 20px;
+		border: none;
+		border-radius: 4px;
+		
+	}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+	h1 {
+		width: 100px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	} */
 </style>
